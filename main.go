05e5153f35d7dc379838c5c25eccae920fa569f2
chen chen <cchen0628@qq.com>
2016-03-24 18:34:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	for i := 0; i < 1000000; i++ {
-		fmt.Println("keep calm and carry on -- chenchen")
+		fmt.Println("keep calm and carry on -- chenchen 2")
 		time.Sleep(time.Second * 10)
 	}
 }
